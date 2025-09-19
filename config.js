50f0bd5d1098d272264e60a8006fc0e6c1731d97
BokChii <76834141+BokChii@users.noreply.github.com>
2025-09-19 11:54:29 +0900

    Update config.js
    api 키 업데이트

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // API 키를 여기에 설정하세요.
 // 예: const API_KEY = 'AIza...';
-const API_KEY = 'AIzaSyACzQPvcltn0FlmsrPp1yT3oTgQjVftHf0';
-
+// 구글 본 계정 api 키: AIzaSyACzQPvcltn0FlmsrPp1yT3oTgQjVftHf0
+const API_KEY = 'AIzaSyCGuiWVQgNS8egFImIxqQjA1LFI47TBmw8';
